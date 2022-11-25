ec9074ea3d49c4f5138223515484ea331b0b1787
itwassoclear <itwassoclear@gmail.com>
2022-11-25 16:08:22 +0100

    fix: regExp

--- src/utils/pattern.ts
+++ src/utils/pattern.ts
@@ -10,31 +10,31 @@ export const pattern: Record<string, IPattern> = {
   },
   email: {
     error: "please use any@any.any",
-    regExp: /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/,
+    regExp: /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})$/,
   },
   first_name: {
     error: "3-15 characters from capital letter",
-    regExp: /^[А-ЯA-Z]{1}[а-яa-z0-9_-]{3,15}/,
+    regExp: /^[А-ЯA-Z]{1}[а-яa-z0-9_-]{3,15}$/,
   },
   second_name: {
     error: "3-15 characters from capital letter",
-    regExp: /^[А-ЯA-Z]{1}[а-яa-z0-9_-]{3,15}/,
+    regExp: /^[А-ЯA-Z]{1}[а-яa-z0-9_-]{3,15}$/,
   },
   phone: {
     error: "10-15 numbers",
-    regExp: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,9}/,
+    regExp: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,9}$/,
   },
   chat_name: {
     error: "10-12 characters",
-    regExp: /^[а-яa-zА-ЯA-Z0-9_-]{3,15}/,
+    regExp: /^[а-яa-zА-ЯA-Z0-9_-]{3,15}$/,
   },
   password: {
     error: "8-40 characters (at least 1 number, 1 capital)",
-    regExp: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,40})/,
+    regExp: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,40})$/,
   },
   message: {
     error: "write something",
-    regExp: /(.|\s)*\S(.|\s)*/,
+    regExp: /(.|\s)*\S(.|\s)*$/,
   },
   search: {
     error: "write something",
